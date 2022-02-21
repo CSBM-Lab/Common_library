21eb7267b41713616c3f2720d20285909f23912b
JohnathanL <jagonball@g-mail.nsysu.edu.tw>
2022-02-21 20:27:34 +0800

    update code plot x axis complete change filter from creating a new DataFrame, to drop the unwanted rows

--- Perseus_Functions/PersMatrix_plotter.py
+++ Perseus_Functions/PersMatrix_plotter.py
@@ -14,32 +14,33 @@ from contextlib import suppress
 Transform [NEW DataFrame] x='column name' from string into float, excluding first row
 '''
 def Tf_float(x):
-    x = pd.to_numeric(df_new[x], downcast='float')
+    x = pd.to_numeric(df[x], downcast='float')
     return x
 
 '''
 Reduce [DataFrame] based on 'Category column' with value x (GO or KEGG)
 '''
 def DF_Reduce_Cat(x):
-    df_filtered = df.loc[df['Category column'] == x]
+    df_filtered = df[df['Category column'] == x]
     return df_filtered
 
 '''
 Reduce [DataFrame] based on 'Selection value' with value x (cluster-number)
 '''
 def DF_Reduce_Sele(x):
-    df_filtered = df.loc[df['Selection value'] == x]
+    df_filtered = df[df['Selection value'] == x]
     return df_filtered
 
 '''
 Filter [DataFrame] with goatools to filter out more specific targets,
 keeping rows which [depth - level <= 3]
-Create a [NEW DataFrame] df_new
+Create a [NEW DataFrame] df
 '''
 def Run_filter(col):
-    global Row_num, Row_keep
+    global Row_num, Row_keep, Row_drop
     Row_num = 0 # the row number of GO
     Row_keep = [] # the list of rows to keep
+    Row_drop = [] # the list of rows to drop
     for i in df[col]:
         with suppress(AttributeError): # Skip the AttributeError(GO term is obsolete, thus not found in GO.obo) and continue
             GO_obo(i)
@@ -72,23 +73,33 @@ def GO_obo(GO):
     '''
     if levels == []:
         print('empty')
+        Row_drop.append(Row_num)
     else:
         count = 0
         for index, item in enumerate(levels):
             ##print(depths[index] - levels[index])
-            if depths[index] - levels[index] >= 3:
+            if depths[index] - levels[index] >=  3:
                 count += 1
                 print('>=3')
             else:
                 print('keep row ' + str(Row_num+1))
         if count == 0: # When count is 0, we keep the row from DataFrame
             Row_keep.append(Row_num)
+        else: # When count is not 0, we drop the row from DataFrame
+            Row_drop.append(Row_num)
+
+'''
+Return GO name of the GO term
+'''
+def GO_name(GO):
+    term = obo_parser.GODag('go.obo').query_term(GO)
+    return term.name
 
 '''
 Put DataFrame column 'x' into a list
 '''
 def DF_list(x):
-    x = df_new[x].to_list()
+    x = df[x].to_list()
     return x
 
 if __name__ == '__main__':
@@ -102,7 +113,7 @@ if __name__ == '__main__':
     Cat_input = 'GOMF' ### Decide which Category to use
     df = DF_Reduce_Cat(Cat_input)
     # Reduce DataFrame based on 'Selection value' with Sele_input
-    #Sele_input = 'Cluster-810' ### Decide which Selection to use
+    Sele_input = 'Cluster -810' ### Decide which Selection to use
     #df = DF_Reduce_Sele(Sele_input)
 
     ## Reduce df_all to only one column with name Cat_input, excluding first row
@@ -113,19 +124,35 @@ if __name__ == '__main__':
     #df['Category value'].to_csv('df_GOBP.txt', index=False, sep='\t')
     #df_all_reduced.to_csv('df_all_GOMF.txt', index=False, sep='\t')
 
-    df.dropna(inplace=True) # Drop the rows with (NaN)
+    #df.dropna(inplace=True) # Drop the rows with (NaN)
 
-    Run_filter('Category value')
-    print('Rows to keep:', Row_keep) ### print the list Row_keep to check
-    df_new = df.iloc[Row_keep,:]
-    ##print(df_new) ### print the new DataFrame to check
-    
-    ## Add a 'Gene Ratio'(Intersection size / Category size) column. !!!First row will be empty (NaN)!!!
-    #df_new.loc['Intersection size'] = Tf_float('Intersection size')
-    #df_new.loc['Category size'] = Tf_float('Category size')
-    #df_new['Gene ratio'] = df_new.loc['Intersection size'] / df_new.loc['Category size']
+    # Filter the data if the Category selected is GO term
+    if Cat_input == 'KEGG':
+        df = df
+    else:
+        Run_filter('Category value')
+        print('Rows to keep:', Row_keep) ### print the list Row_keep to check
+        print('Rows to drop:', Row_drop) ### print the list Row_drop to check
+        df.drop(df.index[Row_drop], inplace=True)
+        #df = df.iloc[Row_keep,:]
+
+
+        # Replace name with GO term
+        for index, GO in enumerate(df['Category value']):
+            print(index)
+            print(GO)
+            #print(df.loc[index, 'Category value'])
+            #df.loc[index, 'Category value'] = GO_name(GO)
+            #print(df.loc[index, 'Category value'])
+
+    ##print(df) ### print the new DataFrame to check
+
+    # Add a 'Gene Ratio'(Intersection size / Category size) column. !!!First row will be empty (NaN)!!!
+    df['Intersection size'] = Tf_float('Intersection size')
+    df['Category size'] = Tf_float('Category size')
+    df['Gene ratio'] = df['Intersection size'] / df['Category size']
     '''This df is ready for plot drawing'''
-    df_new.to_csv('df_GOMF_filtered.txt', index=False, sep='\t') ### Create a file to check
+    df.to_csv('df_GOMF_filtered.txt', index=False, sep='\t') ### Create a file to check
     
     '''Let's begin to draw the plot'''
     # Plot parameters from [DataFrame] column
@@ -139,11 +166,11 @@ if __name__ == '__main__':
 
     # Transform string into float and modify marker size
     M_size = 1 ### 500 for 'Gene ratio', 1 for 'Intersection size'
-    df_new[x_input] = Tf_float(x_input)
-    df_new[c_input] = Tf_float(c_input)
-    df_new[s_input] = M_size * Tf_float(s_input)
-    df_new.dropna(inplace=True) # Drop the rows with (NaN)
-    ##df_new.to_csv('df_GOMF_dropped.txt', index=False, sep='\t') ### Create a file to check
+    df[x_input] = Tf_float(x_input)
+    df[c_input] = Tf_float(c_input)
+    df[s_input] = M_size * Tf_float(s_input)
+    df.dropna(inplace=True) # Drop the rows with (NaN)
+    ##df.to_csv('df_GOMF_dropped.txt', index=False, sep='\t') ### Create a file to check
 
     # Make data into a list (not necessary for x and y)
     x = DF_list(x_input)
@@ -156,7 +183,7 @@ if __name__ == '__main__':
     print('c:', c)
 
     # setup plot and draw the scatter plot
-    fig, ax = plt.subplots(figsize=(5, 5)) ### Decide plot size
+    fig, ax = plt.subplots() ### Decide plot size (figsize=(5, 5))
     sc = ax.scatter(x, y, s, c, cmap='coolwarm')
 
     # Set plot margins, Title and labels
@@ -167,10 +194,11 @@ if __name__ == '__main__':
 
     # Set up tick locator on x axis
     #ax.invert_xaxis()
-    ax.set_xticks(np.arange(1, 4, 0.5))
+    x_min = float(np.floor(min(x))) ### Decide lowest x tick from the min x
+    x_max = float(np.ceil(max(x))) ### Decide highest x tick from the max x
+    ax.set_xticks(np.arange(x_min, x_max+0.5, 0.5))
     #ax.set_xticklabels(['0', '1', '2', '3', '4'])
     #ax.xaxis.set_major_locator(ticker.MaxNLocator(5, integer=True))
-    #ax.invert_xaxis()
     #plt.xticks(range(1,3))
     #plt.gca().xaxis.set_major_locator(ticker.MultipleLocator(1))
     #ax.xaxis.set_major_formatter(ticker.StrMethodFormatter('{x:.2f}')) 
@@ -202,6 +230,7 @@ if __name__ == '__main__':
             s_num += 1
     print('legend_values:', legend_values)
 
+    plt.tight_layout() ### Rescale the fig size to fit the data
     # Get the bounds of colorbar axis
     xmin, ymin, dx, dy = cbar.ax.get_position().bounds
     ##print(xmin)
@@ -245,6 +274,6 @@ if __name__ == '__main__':
         sax.spines[pos].set_visible(False)
 
 
-    # plt.tight_layout()
-    plt.savefig('DotPlot_GO.png',bbox_inches='tight')
+
+    plt.savefig('DotPlot_GO.png')
     #plt.show()
--- Perseus_Functions/parse_go.py
+++ Perseus_Functions/parse_go.py
@@ -1,6 +1,7 @@
 from goatools import obo_parser
 
-term = obo_parser.GODag('go.obo').query_term('GO:0003729')
+term = obo_parser.GODag('go.obo').query_term('GO:0045259')
 print(term.parents)
 print(term.level)
 print(term.depth)
+print(term.name)
